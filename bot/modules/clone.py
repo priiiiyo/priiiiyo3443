8d9bc80f9824c94325c326f98dfc408454141c23
S M Mahabub Hossain <77372461+priiiiyo@users.noreply.github.com>
2022-07-08 10:13:16 +0600

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -176,9 +176,9 @@ def _clone(message, bot, multi=0):
             pmwarn = ""
         uploadmsg = sendMarkup(result + cc + pmwarn + warnmsg, bot, message, button)
         Thread(target=auto_delete_upload_message, args=(bot, message, uploadmsg).start()
-        if MIRROR_LOGS
-                for i in MIRROR_LOGS:
-                    bot.sendMessage(chat_id=i, text=result + cc, reply_markup=button, parse_mode=ParseMode.HTML)
+        if MIRROR_LOGS:
+            for i in MIRROR_LOGS:
+                bot.sendMessage(chat_id=i, text=result + cc, reply_markup=button, parse_mode=ParseMode.HTML)
             except Exception as e:
                 LOGGER.warning(e)
             if BOT_PM and message.chat.type != "private":
